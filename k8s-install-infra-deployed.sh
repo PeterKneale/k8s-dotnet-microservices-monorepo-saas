ae06a7646313a235c9235374f5270da03500ac59
peterkneale <peterkneale@gmail.com>
2022-04-02 18:33:26 +1100

    tidied install script

--- k8s-install-infra-deployed.sh
+++ k8s-install-infra-deployed.sh
@@ -1,16 +1,37 @@
 #!/bin/bash -e
 
-# add repos
+echo "🔵 installing..."
+
+echo "✨ configuring helm repositories"
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
-# Pre-requisites
-kubectl apply -f ./deploy/infra-fluentd-config.yaml
+for env in saas-infra-development saas-infra-production
+do
+    echo "🔵 installing $env"
 
-# Infrastructure to install
-helm install infra-postgresql       bitnami/postgresql      -f ./deploy/infra-postgresql.yaml
-helm install infra-rabbitmq         bitnami/rabbitmq        -f ./deploy/infra-rabbitmq.yaml
-helm install infra-elasticsearch    bitnami/elasticsearch   -f ./deploy/infra-elasticsearch.yaml
-helm install infra-kibana           bitnami/kibana          -f ./deploy/infra-kibana.yaml
-helm install infra-minio            bitnami/minio           -f ./deploy/infra-minio.yaml
-helm install infra-fluentd          bitnami/fluentd         -f ./deploy/infra-fluentd.yaml
+    echo "✨ creating namespaces"
+    kubectl create namespace $env --dry-run=client -o yaml | kubectl apply -f -
+
+    echo "✨ creating secrets"    
+    kubectl delete secret github --namespace $env --ignore-not-found=true
+
+    kubectl create secret docker-registry github \
+        --namespace $env \
+        --docker-server=$REGISTRY_HOST \
+        --docker-username=$GITHUB_USERNAME \
+        --docker-password=$GITHUB_TOKEN
+    
+    echo "✨ creating configs"
+    kubectl apply --namespace $env -f ./deploy/infra-fluentd-config.yaml
+        
+    echo "✨ creating infra"
+    helm upgrade --install --namespace $env infra-postgresql       bitnami/postgresql      -f ./deploy/infra-postgresql.yaml
+    helm upgrade --install --namespace $env infra-rabbitmq         bitnami/rabbitmq        -f ./deploy/infra-rabbitmq.yaml
+    helm upgrade --install --namespace $env infra-elasticsearch    bitnami/elasticsearch   -f ./deploy/infra-elasticsearch.yaml
+    helm upgrade --install --namespace $env infra-kibana           bitnami/kibana          -f ./deploy/infra-kibana.yaml
+    helm upgrade --install --namespace $env infra-minio            bitnami/minio           -f ./deploy/infra-minio.yaml
+    helm upgrade --install --namespace $env infra-fluentd          bitnami/fluentd         -f ./deploy/infra-fluentd.yaml
+    
+    echo "✔ Configured environment: $env"
+done
