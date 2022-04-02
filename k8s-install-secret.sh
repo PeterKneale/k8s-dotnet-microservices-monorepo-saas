04a88f522027f2964f03c078221b5d27b79f0357
peterkneale <peterkneale@gmail.com>
2022-04-02 18:03:35 +1100

    Create indepentent secrets for dev and prod
    #1

--- k8s-install-secret.sh
+++ k8s-install-secret.sh
@@ -1,5 +1,23 @@
 #!/bin/bash -e
 
-# Install Github Pull Credentials
-kubectl delete secret github --ignore-not-found=true
-kubectl create secret docker-registry github --docker-server=$REGISTRY_HOST --docker-username=$GITHUB_USERNAME --docker-password=$GITHUB_TOKEN
+# Remove old github credentials
+kubectl delete secret github \
+    --namespace saas-app-development \
+    --ignore-not-found=true
+kubectl delete secret github \
+    --namespace saas-app-production \
+    --ignore-not-found=true
+
+# Install Github Pull Credentials (Development)
+kubectl create secret docker-registry github \
+    --namespace saas-app-development \
+    --docker-server=$REGISTRY_HOST \
+    --docker-username=$GITHUB_USERNAME \
+    --docker-password=$GITHUB_TOKEN
+    
+# Install Github Pull Credentials (Production)
+kubectl create secret docker-registry github \
+    --namespace saas-app-production \
+    --docker-server=$REGISTRY_HOST \
+    --docker-username=$GITHUB_USERNAME \
+    --docker-password=$GITHUB_TOKEN
