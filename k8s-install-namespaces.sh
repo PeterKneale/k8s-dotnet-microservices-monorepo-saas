5aefb743eee7d658142ea26943874148f5804e19
peterkneale <peterkneale@gmail.com>
2022-04-02 18:37:26 +1100

    remove old mechanism for creating namespaces

--- k8s-install-namespaces.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-# Create namespaces for frontends, backends and services
-kubectl create namespace saas-app-development --dry-run=client -o yaml | kubectl apply -f -
-kubectl create namespace saas-app-production --dry-run=client -o yaml | kubectl apply -f -
-
-# Create namespaces for infrastructure like postgres, rabbitmq and minio
-kubectl create namespace saas-infra-development --dry-run=client -o yaml | kubectl apply -f -
-kubectl create namespace saas-infra-production --dry-run=client -o yaml | kubectl apply -f -
