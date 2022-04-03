c4588800fe3ab4207809b8ceb23f5b2840b471cd
peterkneale <peterkneale@gmail.com>
2022-04-03 18:18:18 +1000

    tidy

--- k8s-uninstall-app.sh
+++ k8s-uninstall-app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-helm ls --short | grep service | xargs -L1 helm delete
-helm ls --short | grep frontend | xargs -L1 helm delete
-helm ls --short | grep backend | xargs -L1 helm delete
+helm ls --short --namespace development | grep service | xargs -L1 helm delete --namespace development
+helm ls --short --namespace development | grep frontend | xargs -L1 helm delete --namespace development
+helm ls --short --namespace development | grep backend | xargs -L1 helm delete --namespace development
